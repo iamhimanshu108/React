48825e70b15c90878afd736a4f58eb15ee96f8f8
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2024-03-23 18:27:48 +0530

    Pokemon

--- pokedex/src/components/PokemonLIst/PokemonList.jsx
+++ pokedex/src/components/PokemonLIst/PokemonList.jsx
@@ -1,27 +1,51 @@
 import { useEffect, useState } from "react"
+import axios from "axios";
+import './PokemonList.css';
+import Pokemon from "../Pokemon/Pokemon";
+
 
 function PokemonList() {
 
+    const [pokemonList, setPokemonList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
+    async function dowmloadPokemons() {
+        const response  = await axios.get('https://pokeapi.co/api/v2/pokemon');
+        const pokemonResults = response.data.results;
+        const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url)) ;  
+        const pokemonData = await axios.all(pokemonResultPromise);
+        console.log(pokemonData);
+
+        const res = setPokemonList(pokemonData.map((pokedata) => {
+            const pokemon = pokedata.data;
+
+            return {
+                id: pokemon.id,
+                name : pokemon.name, 
+                image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny, 
+                types: pokemon.types}
+        }));
+        console.log(res);
+        setPokemonList(res);
+        setIsLoading(false);
+    }
 
     useEffect(() => {
-        console.log("effect called");
+        dowmloadPokemons();
     }, [])
 
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
-
-
+    
 
     return (
-        <>
-        <div>
-            X : {x} <button onClick={() => setX(x+1)}>INC</button>
+        
+        <div className="pokemon-list-wrapper">
+           <div>Pokemon List </div>
+
+           {(isLoading) ? 'Loading...' : 
+                pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} />)}
         </div>
-        <div>
-            Y : {y} <button onClick={() => setX(y+1)}>INC</button>
-        </div>
-        </>
+        
+        
     )
 }
 
