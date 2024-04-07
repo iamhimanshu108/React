03f14df1785e2610fb8b4a53de1fdad20d647748
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-04-07 19:19:46 +0530

    Simple Card with API

---
+++ card/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='div'>
+      <h1>Cards</h1>
+      <div className='nav-links'>
+        <ul>
+            <li>
+            <a href="/">Home</a>
+            </li>
+            <li>
+            <a href="/">Home</a>
+            </li>
+            <li>
+            <a href="/">Home</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
